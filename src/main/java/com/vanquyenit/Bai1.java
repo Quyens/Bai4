75b9fc7c1acbce88cbd93ea5a20cc4e472c8f3c4
quyen <quyen11072k5@gmail.com>
2024-03-28 13:22:15 +0700

    Initial commit

---
+++ src/main/java/com/vanquyenit/Bai1.java
@@ -0,0 +1,7 @@
+package com.vanquyenit;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/vanquyenit/Bai2.java
@@ -0,0 +1,2 @@
+package com.vanquyenit;public class Bai2 {
+}
